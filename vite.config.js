bdd1b42847a23e12bf445981dd0bfe84db510f3f
qvissak <qvissak@yahoo.com>
2026-08-09 13:53:08 -0400

    (fix) base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/', // Important for GitHub Pages routing
+  base: '/kevingutzman/', // Important for GitHub Pages routing
 })
